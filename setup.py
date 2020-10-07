3b26bf9acb7d6d94da2748ea6cde24429df23070
Jerome Guibert <jguibert@gmail.com>
2020-10-07 19:58:16 +0200

    add long description from readme, set apache licence

--- setup.py
+++ setup.py
@@ -3,18 +3,25 @@
 from setuptools import setup, find_packages
 import versioneer
 
+with open('README.rst', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='pytest-curio',
     version=versioneer.get_version(),
     packages=find_packages(),
     url='https://github.com/johnnoone/pytest-curio',
-    license='MIT',
+    license='Apache License 2.0',
     author='Xavier Barbosa',
     author_email='clint.northwood@gmail.com',
     description='Pytest support for curio.',
+    long_description=long_description,
     classifiers=[
         "Intended Audience :: Developers",
+        "Framework :: Curio",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Testing",
     ],
     install_requires=[
